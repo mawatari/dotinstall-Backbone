3800b801224c088c79181993f3ee38d030330da9
Naoto Mawatari <github.mawatari@gmail.com>
2013-06-27 16:12:28 +0900

    #03 Modelを作ってみよう Modelの作り方、defaultsの指定や、toJSON()メソッド等

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,17 @@
 (function() {
+    // Model
+    var Task = Backbone.Model.extend({
+        // defaults の指定もできる
+        defaults: {
+            title: 'do something!',
+            completed: false
+        }
+    });
+    
+    var task = new Task({
+        completed: true
+    });
 
+    // toJSON() とすれば、attributesプロパティのみを返してくれる
+    console.log(task.toJSON());
 })();
