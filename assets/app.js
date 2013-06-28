31ca7b601e4c1013111879438b6c0409989ea160
Naoto Mawatari <github.mawatari@gmail.com>
2013-06-28 11:02:42 +0900

    #07 render、templateを使ってみよう renderメソッドの定義と、テンプレートの使い方 テンプレートは、Underscore.jsを利用

--- assets/app.js
+++ assets/app.js
@@ -11,12 +11,27 @@
 
     // View
     var TaskView = Backbone.View.extend({
-        tagName: 'li'
+        tagName: 'li',
+        // class, id を付与する場合は以下の通り
+        className: 'liClass',
+        id: 'liId',
+
+        // templateを定義する
+        // クオートはダブルとシングルで意味が変わるので注意
+        template: _.template("<%- title %>"),
+        
+        // renderを定義
+        // modelをJSONで得て、テンプレートにはめる
+        // ViewのエレメントにHTMLとして挿入
+        // return thisをしておくのが作法。メソッドチェーン等を使うときに恩恵を受けれる
+        render: function() {
+            var template = this.template(this.model.toJSON());
+            this.$el.html(template);
+            return this;
+        }
     });
 
     var taskView = new TaskView({model: task});
-    // インスタンスのエレメントだけを表示する場合は、elプロパティを指定する
-    console.log(taskView.el);
-    // $をつけるとjQueryのオブジェクトになるので、append等が使える
-    console.log(taskView.$el);
+    // renderメソッドを実行し、その中のエレメントを得る
+    console.log(taskView.render().el);
 })();
