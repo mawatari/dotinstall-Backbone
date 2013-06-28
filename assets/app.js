a88249975b6da2797a58a23c2ea2447f4881b65a
Naoto Mawatari <github.mawatari@gmail.com>
2013-06-28 09:45:04 +0900

    #05 validateを使ってみよう validateが効いているため、30行目のset()メソッドが受け入れられず、28行目と31行目の結果が変わっていないことが分かる エラーメッセージの表示については今後

--- assets/app.js
+++ assets/app.js
@@ -5,12 +5,17 @@
             title: 'do something!',
             completed: false
         },
+        validate: function(attrs) {
+            if (_.isEmpty(attrs.title)) {
+                return "title must not be empty!";
+            }
+        },
         // メソッドのようなものも作れる
         toggle: function() {
             this.set('completed', !this.get('completed'));
         }
     });
-    
+
     var task = new Task({
         completed: true
     });
@@ -21,8 +26,9 @@
 //    console.log(title);
 
     console.log(task.toJSON());
-    task.toggle();
+//    task.toggle();
+    task.set({title: ''}, {validate: true});
     console.log(task.toJSON());
-    task.toggle();
-    console.log(task.toJSON());
+//    task.toggle();
+//    console.log(task.toJSON());
 })();
