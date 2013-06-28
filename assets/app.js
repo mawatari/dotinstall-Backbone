2e91392739243d6865185eb57a6789b21a7215aa
Naoto Mawatari <github.mawatari@gmail.com>
2013-06-28 10:06:08 +0900

    #06 Viewを作ってみよう Viewの作り方と簡単な確認の仕方等

--- assets/app.js
+++ assets/app.js
@@ -4,31 +4,19 @@
         defaults: {
             title: 'do something!',
             completed: false
-        },
-        validate: function(attrs) {
-            if (_.isEmpty(attrs.title)) {
-                return "title must not be empty!";
-            }
-        },
-        // メソッドのようなものも作れる
-        toggle: function() {
-            this.set('completed', !this.get('completed'));
         }
     });
 
-    var task = new Task({
-        completed: true
+    var task = new Task();
+
+    // View
+    var TaskView = Backbone.View.extend({
+        tagName: 'li'
     });
 
-    // set, get
-//    task.set('title', 'newTitle');
-//    var title = task.get('title');
-//    console.log(title);
-
-    console.log(task.toJSON());
-//    task.toggle();
-    task.set({title: ''}, {validate: true});
-    console.log(task.toJSON());
-//    task.toggle();
-//    console.log(task.toJSON());
+    var taskView = new TaskView({model: task});
+    // インスタンスのエレメントだけを表示する場合は、elプロパティを指定する
+    console.log(taskView.el);
+    // $をつけるとjQueryのオブジェクトになるので、append等が使える
+    console.log(taskView.$el);
 })();
