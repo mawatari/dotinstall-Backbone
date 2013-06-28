9a4e4a2bf61337266c1574ae5b1b078eea22e92a
Naoto Mawatari <github.mawatari@gmail.com>
2013-06-28 18:55:54 +0900

    #15 taskの削除処理を作っていこう Viewから削除するだけでなく、Modelもdestroyする

--- assets/app.js
+++ assets/app.js
@@ -9,6 +9,25 @@
 
     var TaskView = Backbone.View.extend({
         tagName: 'li',
+        // インスタンス生成時にコールされる
+        initialize: function() {
+            // on: イベントが発火した際に指定したコールバック関数を実行する
+            this.model.on('destroy', this.remove, this);
+        },
+        events: {
+            // .deleteクラスをクリックした際にdestroyメソッドを実行する
+            'click .delete': 'destroy'
+        },
+        destroy: function() {
+            if (confirm('削除してもよいですか？')) {
+                // okの場合、modelのdestroyメソッドを実行する
+                this.model.destroy();
+            }
+        },
+        // onメソッドにより、modelのdestroy実行時にコールバックとして実行される
+        remove: function() {
+            this.$el.remove();
+        },
         template: _.template($('#task-template').html()),
         render: function() {
             var template = this.template(this.model.toJSON());
